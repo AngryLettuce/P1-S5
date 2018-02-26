ae60ec4a3fd300e952606ba3e87ca32e7127bc7c
Guillaume Roux <guillaume03@live.ca>
2018-02-26 15:04:47 -0500

    P1-111 Modification des variables globals et du protocol virtuel

--- Communication UART/ProjetS5.py
+++ Communication UART/ProjetS5.py
@@ -1,4 +1,6 @@
-#Created by Guillaume Roux, this project aim to make a UART communication with a PIC and to make a graphical interface showing the picture of someone
+#Created by Guillaume Roux, 
+#this project aim to make a sserial communication with a PIC
+# and to make a graphical interface showing the picture of someone
 
 
 import tkinter as tk   
@@ -6,10 +8,9 @@ from PIL import Image, ImageTk
 import os
 import serial
 from time import sleep
+import threading
 
-
-count = 0 
-
+    
 def ImageDictionnary(Orateur):
     Dict = {"Guillaume" : r"C:\Users\Guill\Pictures\Death_of_Pablo_Escobar.jpg",
             "Pascal"    : r"C:\Users\Guill\Pictures\Pascal.PNG"
@@ -20,75 +21,67 @@ def ImageDictionnary(Orateur):
     return photo
 
 
-
-def CycleImage():
-    global count
-
-    if count == 0 : 
-        OrateurVariable = "Guillaume"
-        count = 1
-    else :
-        OrateurVariable = "Pascal"
-        count = 0 
-
-    photo = ImageDictionnary(OrateurVariable)
-    OrateurPicLabel.configure(image=photo)
-    OrateurPicLabel.photo = photo
-
-def setupSerialPort(port):
-    ser = serial.Serial(port, 38400, timeout=0.001)
-    return ser
-
-def readSerial():
-    data = ser2.read(9999)
-    test =  data.decode('ascii')
-    ReadingLabel.configure(text=data.decode('ascii'))
-    ReadingLabel.text = data.decode('ascii')
-
-def writeSerial():
-    data = writingBox.get()
-    ser1.write(data.encode())
-
-    readSerial()
-
 class Application(tk.Frame):         
-    global ser1
-    global ser2
-
-    ser1 = setupSerialPort("\\\\.\\CNCA0")
-    ser2 = setupSerialPort("\\\\.\\CNCB0")
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)  
         self.grid()                      
         self.createWidgets()
-
+        self.ser1 = self.setupSerialPort("\\\\.\\CNCA0")
+        self.ser2 = self.setupSerialPort("\\\\.\\CNCB0")
+        self.CyclePhoto = 0
 
     def createWidgets(self):
-        self.quitButton = tk.Button(self,  text='Exit',     command=self.quit)         
-        self.quitButton.grid()   
+        #self.quitButton = tk.Button(self,  text='Exit',     command=self.quit)         
+        #self.quitButton.grid(row=1, column=3)   
 
-        self.TrainButton = tk.Button(self, text='Training', command=lambda: CycleImage())            
-        self.TrainButton.grid()
+        self.TrainButton = tk.Button(self, text='Training', command=lambda: self.CycleImage())            
+        self.TrainButton.grid(row=1, column=1)
 
-        #self.ReadSerial = tk.Button(self,  text='Read Serial',  command=lambda: readSerial())
-        #self.ReadSerial.grid()
+        self.ReadSerial = tk.Button(self,  text='Read Serial',  command=lambda: self.readSerial())
+        self.ReadSerial.grid()
 
-        self.WriteSerial = tk.Button(self, text='Write Serial', command=lambda: writeSerial())
-        self.WriteSerial.grid()
+        self.WriteSerial = tk.Button(self, text='Write Serial', command=lambda: self.writingSerialButton())
+        self.WriteSerial.grid(row=1, column=2)
 
-        global writingBox
-        writingBox = tk.Entry(self)
-        writingBox.grid()
+        self.writingBox = tk.Entry(self)
+        self.writingBox.grid(columnspan=4, row=2, column=1)
 
-        global ReadingLabel
-        ReadingLabel = tk.Label(self)
-        ReadingLabel.grid()
+        self.ReadingLabel = tk.Label(self)
+        self.ReadingLabel.grid(columnspan=4, row=3, column=4)
 
-        global OrateurPicLabel
-        OrateurPicLabel = tk.Label(self)
-        OrateurPicLabel.grid()
+        self.OrateurPicLabel = tk.Label(self)
+        self.OrateurPicLabel.grid(columnspan=15, row=4, column=5)
 
+    def setupSerialPort(self, port):
+        ser = serial.Serial(port, 38400, timeout=0.000001)
+        return ser
+
+    def CycleImage(self):
+        if self.CyclePhoto == 0 : 
+            OrateurVariable = "Guillaume"
+            self.CyclePhoto = 1
+        else :
+            OrateurVariable = "Pascal"
+            self.CyclePhoto = 0 
+
+        photo = ImageDictionnary(OrateurVariable)
+        self.OrateurPicLabel.configure(image=photo)
+        self.OrateurPicLabel.photo = photo
+
+    def readSerial(self):
+        data = self.ser2.read(9999)
+        self.ReadingLabel.configure(text=data.decode('ascii'))
+        self.ReadingLabel.text = data.decode('ascii')
+
+    def writeSerial(self):
+        data = self.writingBox.get()
+        self.ser1.write(data.encode())
+
+
+    def writingSerialButton(self):
+        self.writeSerial()
+        #self.readSerial()
 
 
 
@@ -100,10 +93,16 @@ class Application(tk.Frame):
 
 
 
+MyRoot = tk.Tk()
 
-app = Application()                       
+app = Application(MyRoot)                       
 app.master.title('Projet S5 P01')    
+
+w = MyRoot.winfo_screenwidth()
+h = MyRoot.winfo_screenheight()
+MyRoot.geometry("%dx%d+0+0" % (w, h))
+
 app.mainloop()
    
-ser1.close()
-ser2.close()
+app.ser1.close()
+app.ser2.close()
