97c8b33ad581bb533f7447aef4d113fc4a2171ea
Guillaume Roux <guillaume03@live.ca>
2018-03-15 15:36:30 -0400

    P1-109
    ajout d'une fonction pour retravailler

--- Communication UART/ProjetS5.py
+++ Communication UART/ProjetS5.py
@@ -10,6 +10,25 @@ import serial
 from time import sleep
 from threading import Timer
 
+def resizePic(size, path) :
+    basewidth = size
+    img = Image.open(path)
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img.save('sompic.jpg') 
+
+def ImageDictionnary(Orateur):
+    Dict = {'1' : r"Death_of_Pablo_Escobar.jpg",
+            '2' : r"Pascal.PNG",
+            '3' : r"Pascal.JPG"
+            } 
+
+    image = Image.open(Dict[Orateur])
+    photo = ImageTk.PhotoImage(image)
+
+    return photo
+
 class RepeatedTimer(object):
     def __init__(self, interval, function, *args, **kwargs):
         self._timer     = None
@@ -36,16 +55,6 @@ class RepeatedTimer(object):
         self.is_running = False
 
     
-def ImageDictionnary(Orateur):
-    Dict = {"Guillaume" : r"C:\Users\Guill\Pictures\Death_of_Pablo_Escobar.jpg",
-            "Pascal"    : r"C:\Users\Guill\Pictures\Pascal.PNG"
-            } 
-    image = Image.open(Dict[Orateur])
-    photo = ImageTk.PhotoImage(image)
-
-    return photo
-
-
 class ApplicationProjetS5(tk.Frame):         
 
 
@@ -56,14 +65,14 @@ class ApplicationProjetS5(tk.Frame):
         tk.Frame.__init__(self, master)  
         self.grid()                      
         self.createWidgets()
-        self.ser1 = self.setupSerialPort("COM2", baurate, readingTimeout)
-        self.ser2 = self.setupSerialPort("COM3", baurate, readingTimeout)      
+
+        #self.ser1 = self.setupSerialPort("COM2", baurate, readingTimeout)
+        #self.ser2 = self.setupSerialPort("COM3", baurate, readingTimeout)      
 
         self.readingThread = RepeatedTimer(0.001, self.readSerial)
 
-        #self.ser1 = self.setupSerialPort("\\\\.\\CNCA0", baurate, readingTimeout)
-        #self.ser2 = self.setupSerialPort("\\\\.\\CNCB0", baurate, readingTimeout)
-        self.cyclePhoto = 0
+        self.ser1 = self.setupSerialPort("\\\\.\\CNCA0", baurate, readingTimeout)
+        self.ser2 = self.setupSerialPort("\\\\.\\CNCB0", baurate, readingTimeout)
 
     def createWidgets(self):
         #self.quitButton = tk.Button(self,  text='Exit',     command=self.quit)         
@@ -77,14 +86,14 @@ class ApplicationProjetS5(tk.Frame):
         self.writingBox.grid(columnspan=1, row=1, column=6)
 
         self.readSerial_B = tk.Button(self,  text='Read', command=lambda: self.startThread(self.readingThread))
-        self.readSerial_B.grid(columnspan=1, row=4, columns=2)
+        self.readSerial_B.grid(columnspan=1, row=2, columns=2)
         self.readSerial_B.config( height = 3, width = 10 )
 
         self.stopReading_B = tk.Button(self,  text='Stop', command=lambda: self.stopThread(self.readingThread))
-        self.stopReading_B.grid(columnspan=1, row=5, columns=2)
+        self.stopReading_B.grid(columnspan=1, row=3, columns=2)
         self.stopReading_B.config( height = 3, width = 10 )
 
-        self.readingLabel = tk.Label(self, text='This is the reading Label')
+        self.readingLabel = tk.Label(self, text='Statut du DSK : ')
         self.readingLabel.grid(columnspan=1, row=1, column=7)
 
         #self.trainButton_B = tk.Button(self, text='Training', command=lambda: self.cycleImage())            
@@ -97,24 +106,34 @@ class ApplicationProjetS5(tk.Frame):
         ser = serial.Serial(port, baudrate, timeout=timeout)
         return ser
 
-    def cycleImage(self):
-        if self.cyclePhoto == 0 : 
-            OrateurVariable = "Guillaume"
-            self.cyclePhoto = 1
-        else :
-            OrateurVariable = "Pascal"
-            self.cyclePhoto = 0 
+    def cycleImage(self, index):
+        #if self.cyclePhoto == 0 : 
+        #    OrateurVariable = "Guillaume"
+        #    self.cyclePhoto = 1
+        #else :
+        #    OrateurVariable = "Pascal"
+        #    self.cyclePhoto = 0 
 
-        photo = ImageDictionnary(OrateurVariable)
+        self.orateurIndex = index
+        photo = ImageDictionnary(index)
         self.orateurPicLabel.configure(image=photo)
         self.orateurPicLabel.photo = photo
 
     def readSerial(self):
         data = self.ser2.read(9999)
         data = data.decode('ascii')
-        if data != '':
+
+        if 'ind' in data :
+            self.cycleImage(data[3:])
+
+        elif 'status' in  data : 
+            self.readingLabel.configure(text= 'Statut du DSK : ' + data[6:])
+            self.readingLabel.text = 'Statut du DSK : ' + data[6:]
+
+        elif data != '':
             self.readingLabel.configure(text=data)
             self.readingLabel.text = data
+
 
     def writingSerial(self):
         data = self.writingBox.get()
@@ -123,7 +142,7 @@ class ApplicationProjetS5(tk.Frame):
 
     def writingSerialButton(self):
         self.writingSerial()
-        #self.readSerial()
+
 
     def stopThread(self, thread):
         thread.stop()
--- LogBookScript/JsonFormat.py
+++ LogBookScript/JsonFormat.py
@@ -62,7 +62,7 @@ def formatjsonLine(jsonline) :
 				############## |_|  |_/_/ \_\___|_|\_| ##############
 
 
-slackExportPath = r"C:\Users\Guill\Downloads\P1-S5 Slack export Feb 26 2018"
+slackExportPath = r"C:\Users\Guill\Downloads\P1-S5 Slack export Mar 15 2018"
 directoryFolder = os.listdir(slackExportPath)
 logbooks = [folder for folder in directoryFolder if "logbook" in folder]
 doc = Document(default_filepath = "LogBook" )
@@ -74,7 +74,7 @@ for logbook in logbooks :
     jsonfiles = os.listdir(slackExportPath + '\\' + logbook)
     for jsonfile in jsonfiles :
         jsontotalLines.extend(readJsonFile(slackExportPath + '\\' + logbook + '\\' + jsonfile))
-
+         
     with doc.create(Section("Logbook de " + logBookUser)) : 
         for line in jsontotalLines : 
             if 'text' in line : 
