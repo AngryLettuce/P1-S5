ed5b9f72b6589580340baf930fed223d2e67b99e
Guillaume Roux <guillaume03@live.ca>
2018-03-19 15:40:54 -0400

    P1-107 Ajout du label du nom de l'orateur

--- Communication UART/ProjetS5.py
+++ Communication UART/ProjetS5.py
@@ -12,16 +12,22 @@ from threading import Timer
 
 
 def ImageDictionnary(Orateur):
-    Dict = {'0' : r"noImage.jpg",
-            '1' : r"Death_of_Pablo_Escobar.jpg",
-            '2' : r"Pascal.PNG",
-            '3' : r"Pascal.jpg",
+    #Picture size : 381 * 285 px
+    Dict = {'0' : (r"noImage.jpg",       'Inconnu'),
+            '1' : (r"Pablo_Escobar.jpg", 'Pablo Escobar'),
+            '2' : (r"Pascal.PNG",        'Pascal L.'),
+            '3' : (r"Pascal.jpg",        'Pascal L.'),
+            '4' : (r"Guillaume.jpg",     'Guillaume'),
+            '5' : (r"Pascal_B.jpg",      'Pascal B.'),
+            '6' : (r"Guillaume2.jpg",    'Guillaume'),
+            '7' : (r"P_Y.jpg",           'Pierre-Yves'), 
+
             } 
 
-    image = Image.open(Dict[Orateur])
-    photo = ImageTk.PhotoImage(image)
+    if Orateur not in Dict :
+        Orateur = '0'
 
-    return photo
+    return Dict[Orateur]
 
 
 class RepeatedTimer(object):
@@ -60,48 +66,51 @@ class ApplicationProjetS5(tk.Frame):
 
         tk.Frame.__init__(self, master)  
 
+        self.buttonFrame = tk.Frame(self)
         self.bottomFrame = tk.Frame(self)
 
         self.pack()
+        self.buttonFrame.pack(side='bottom')
         self.bottomFrame.pack(side='bottom')         
         
         self.createWidgets()
 
-        self.ser1 = self.setupSerialPort("COM2", baurate, readingTimeout)
-        self.ser2 = self.setupSerialPort("COM3", baurate, readingTimeout)      
+        #self.ser1 = self.setupSerialPort("COM2", baurate, readingTimeout)
+        #self.ser2 = self.setupSerialPort("COM3", baurate, readingTimeout)      
 
         self.readingThread = RepeatedTimer(readingUARTinterval, self.readSerial)
 
-        #self.ser1 = self.setupSerialPort("\\\\.\\CNCA0", baurate, readingTimeout)
-        #self.ser2 = self.setupSerialPort("\\\\.\\CNCB0", baurate, readingTimeout)
+        self.ser1 = self.setupSerialPort("\\\\.\\CNCA0", baurate, readingTimeout)
+        self.ser2 = self.setupSerialPort("\\\\.\\CNCB0", baurate, readingTimeout)
 
     def createWidgets(self):
 
 
         self.orateurPicLabel = tk.Label(self)
         self.orateurPicLabel.pack()
-        #self.orateurPicLabel.grid(columnspan=15, row=4, column=5)
-        photo = ImageDictionnary('0')
+
+        pathAndName = ImageDictionnary('0')
+        photo = Image.open(pathAndName[0])
+        photo = ImageTk.PhotoImage(photo)
         self.orateurPicLabel.configure(image=photo)
         self.orateurPicLabel.photo = photo
 
+        self.orateurLabel = tk.Label(self, text='Orateur : Inconnu ')
+        self.orateurLabel.pack()
+
         self.readingLabel = tk.Label(self, text='Statut du DSK : ')
         self.readingLabel.pack()
-        #self.readingLabel.grid(columnspan=1, row=1, column=7)
 
-        self.writingSerial_B = tk.Button(self, text='Write', command=lambda: self.writingSerialButton())
+        self.writingSerial_B = tk.Button(self.buttonFrame, text='Write', command=lambda: self.writingSerialButton())
         self.writingSerial_B.pack(side='left')
-        #self.writingSerial_B.grid(columnspan=1, row=1, column=1)
         self.writingSerial_B.config(height = 3, width = 10)
 
-        self.readSerial_B = tk.Button(self,  text='Start', command=lambda: self.startThread(self.readingThread))
+        self.readSerial_B = tk.Button(self.buttonFrame,  text='Start', command=lambda: self.startThread(self.readingThread))
         self.readSerial_B.pack(side='left')
-        #self.readSerial_B.grid(columnspan=1, row=2, columns=2)
         self.readSerial_B.config( height = 3, width = 10 )
 
-        self.stopReading_B = tk.Button(self,  text='Stop', command=lambda: self.stopThread(self.readingThread))
+        self.stopReading_B = tk.Button(self.buttonFrame,  text='Stop', command=lambda: self.stopThread(self.readingThread))
         self.stopReading_B.pack(side='left')
-        #self.stopReading_B.grid(columnspan=1, row=3, columns=2)
         self.stopReading_B.config(height = 3, width = 10 )
 
         self.writingBox = tk.Entry(self.bottomFrame)
@@ -119,32 +128,33 @@ class ApplicationProjetS5(tk.Frame):
         return ser
 
     def cycleImage(self, index):
-        #if self.cyclePhoto == 0 : 
-        #    OrateurVariable = "Guillaume"
-        #    self.cyclePhoto = 1
-        #else :
-        #    OrateurVariable = "Pascal"
-        #    self.cyclePhoto = 0 
 
         self.orateurIndex = index
-        photo = ImageDictionnary(index)
+        pathAndName = ImageDictionnary(index)
+        photo = Image.open(pathAndName[0])
+        photo = ImageTk.PhotoImage(photo)
         self.orateurPicLabel.configure(image=photo)
         self.orateurPicLabel.photo = photo
+
+        return pathAndName
 
     def readSerial(self):
         data = self.ser2.read(9999)
         data = data.decode('ascii')
 
         if 'ind' in data :
-            self.cycleImage(data[3:])
+            pathAndName = self.cycleImage(data[3:])
+            self.orateurLabel.configure(text= 'Orateur : ' + pathAndName[1])
+            self.orateurLabel.text = 'Orateur : ' + pathAndName[1]
 
         elif 'status' in  data : 
             self.readingLabel.configure(text= 'Statut du DSK : ' + data[6:])
             self.readingLabel.text = 'Statut du DSK : ' + data[6:]
 
-        elif data != '':
-            self.readingLabel.configure(text=data)
-            self.readingLabel.text = data
+        #Write what ever is sent
+        #elif data != '':
+        #    self.readingLabel.configure(text=data)
+        #    self.readingLabel.text = data
 
 
     def writingSerial(self):
@@ -180,7 +190,7 @@ app.master.title('Projet S5 P01')
 
 w = MyRoot.winfo_screenwidth()
 h = MyRoot.winfo_screenheight()
-MyRoot.geometry("%dx%d+0+0" % (w/3, h/2)) 
+MyRoot.geometry("%dx%d+0+0" % (w/2.8, h/1.7)) 
 
 app.mainloop()
    
